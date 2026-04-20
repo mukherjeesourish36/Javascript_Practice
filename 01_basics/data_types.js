3825226ab2b077f884e8ce5c8df2c42611b0f19c
mukherjeesourish36 <86368695+mukherjeesourish36@users.noreply.github.com>
2026-04-20 22:01:08 +0000

    data_types

---
+++ 01_basics/data_types.js
@@ -0,0 +1,10 @@
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+
+    console.log(typeof null);
+
+    console.log(typeof undefined);
