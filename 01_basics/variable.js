bd2bc275fa0ba24a0ecd99c7a8bcdbdefb031419
mukherjeesourish36 <86368695+mukherjeesourish36@users.noreply.github.com>
2026-04-20 20:06:38 +0000

    variable

---
+++ 01_basics/variable.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail = "hitesh@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+// accountId = 2 // not allowed
+
+accountEmail = "hc@hc.com"
+accountPassword = "21212121"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
