5ce8243426c1e20268344d5fc4c4e0667bbfcf92
mukherjeesourish36 <86368695+mukherjeesourish36@users.noreply.github.com>
2026-04-20 22:24:33 +0000

    conversion_1

---
+++ 01_basics/datatype_conversion.js
@@ -0,0 +1,12 @@
+let score = "33abc"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
