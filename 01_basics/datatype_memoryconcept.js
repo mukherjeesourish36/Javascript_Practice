64f66ae7c193be2e9f86cb51e041acbb82db2e37
mukherjeesourish36 <86368695+mukherjeesourish36@users.noreply.github.com>
2026-04-21 19:38:38 +0000

    memory

---
+++ 01_basics/datatype_memoryconcept.js
@@ -0,0 +1,16 @@
+let nameOne="Sourish"
+let nameTwo=nameOne
+nameTwo="Sunny"
+
+// console.log(nameOne)
+// console.log(nameTwo)
+
+let detailsUser ={
+    email:"skm@gmail.com",
+    upiId: "sourish-4@okaxis"
+}
+
+let userDetails=detailsUser
+userDetails.email="sunny@gmail.com"
+console.log(detailsUser)
+console.log(userDetails)
