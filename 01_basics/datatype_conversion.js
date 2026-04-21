214efd8f3d892f03f846349add0219576d813e13
mukherjeesourish36 <86368695+mukherjeesourish36@users.noreply.github.com>
2026-04-21 13:51:47 +0000

    2nd commit of coversion

--- 01_basics/datatype_conversion.js
+++ 01_basics/datatype_conversion.js
@@ -1,12 +1,21 @@
 let score = "33abc"
 
-console.log(typeof score);
-console.log(typeof(score));
+//console.log(typeof score);
+//console.log(typeof(score));
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+let isLoggedIn = 1
+let isBoolean = Boolean(isLoggedIn)
+//console.log(isBoolean)
 
 // 1 => true; 0 => false
 // "" => false
 // "hitesh" => true
+
+let someNumber=33
+let checkNumber=String(someNumber)
+console.log(checkNumber)
+console.log(typeof checkNumber)
