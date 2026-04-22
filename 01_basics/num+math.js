615146c8f3b7ea5ddd8b1a44b816e0b0f667e531
mukherjeesourish36 <86368695+mukherjeesourish36@users.noreply.github.com>
2026-04-22 14:15:19 +0000

    random commit

--- 01_basics/num+math.js
+++
@@ -1,7 +0,0 @@
-let score = 400;
-console.log(score);
-console.log(typeof(score));
-
-let balance = new Number(100);
-console.log(balance);
-console.log(typeof(balance));
--- 01_basics/num_math.js
+++ 01_basics/num_math.js
@@ -1,7 +1,30 @@
 let score = 400;
-console.log(score);
-console.log(typeof(score));
+// console.log(score);
+// console.log(typeof(score));
 
 let balance = new Number(100);
-console.log(balance);
-console.log(typeof(balance));
+// console.log(balance);
+// console.log(typeof(balance));
+
+// console.log(balance.toString().length);
+
+// console.log(balance.toFixed(3));
+
+const otherNum = 17.98919
+// console.log(otherNum.toPrecision(3));
+
+const hundred = 100000;
+// console.log(hundred.toLocaleString('en-IN'));
+
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.2));
+// console.log(Math.floor(4.37));
+// console.log(Math.ceil(4.37));
+// console.log(Math.max(4,3,7,9,2));
+// console.log(Math.min(4,3,7,9,2));
+
+// console.log(Math.random());
+// console.log(Math.random()*10);
+// console.log((Math.random()*10)+1);
+
+console.log(Math.floor((Math.random()*10))+10);
