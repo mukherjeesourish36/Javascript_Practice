2e7b11bb56e91797d4e19d504a2620bc297c864f
mukherjeesourish36 <86368695+mukherjeesourish36@users.noreply.github.com>
2026-04-23 10:59:52 +0000

    add this

--- 02_basics/slice_splice.js
+++ 02_basics/slice_splice.js
@@ -1,8 +1,8 @@
 let cars = ['Benz', 'Innova', 'Breeza', 'Etios', 'Dzire'];
 let newCars = cars.slice(1, 4);
 
-console.log(cars);     // ['Benz','Innova','Breeza','Etios','Dzire']
-console.log(newCars);  // ['Innova','Breeza','Etios']
+// console.log(cars);     // ['Benz','Innova','Breeza','Etios','Dzire']
+// console.log(newCars);  // ['Innova','Breeza','Etios']
 
 /*Purpose: Extracts a portion of an array into a new array.
 
@@ -13,3 +13,14 @@ Syntax: array.slice(start, end)
 start → index to begin (inclusive)
 
 end → index to stop (exclusive)*/
+
+let numbers = [1, 2, 3, 4, 5];
+let removed = numbers.splice(2, 2);
+
+// console.log(numbers); // [1, 2, 5]
+// console.log(removed); // [3, 4]
+
+let fruits = ['apple', 'banana', 'cherry'];
+let newFruits = fruits.splice(1, 0, 'mango', 'orange');
+
+console.log(fruits); // ['apple','mango','orange','banana','cherry']
