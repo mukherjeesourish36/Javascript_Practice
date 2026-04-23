9dcfbe4a180ea7d019bc7d41e0b97daebf12c173
mukherjeesourish36 <86368695+mukherjeesourish36@users.noreply.github.com>
2026-04-23 13:38:47 +0000

    com1

--- 03_array/concap_spread_operator.js
+++ 03_array/concap_spread_operator.js
@@ -6,4 +6,27 @@ let all_heroes = marvel_heroes.concat(dc_heroes);
 //console.log(all_heroes);
 
 let all_Heroes = [...marvel_heroes,...dc_heroes];
-console.log(all_Heroes);
+// console.log(all_Heroes);
+
+let demoArray = [1,2,[3,4],[5,[6,7],8,[9,10,11]]]
+
+let flatArray = demoArray.flat(Infinity);
+
+// console.log(flatArray)
+
+// console.log(Array.isArray("Hitesh"))
+// console.log(Array.from("Hitesh"))
+// console.log(Array.from({name: "hitesh"}))
+
+// console.log(Object.keys({name: "hitesh"}))  // ["name"]
+// console.log(Object.values({name: "hitesh"})) // ["hitesh"]
+// console.log(Object.entries({name: "hitesh"}))// [["name", "hitesh"]]
+
+let score1 = 100
+let name = "Sourish"
+let details = {
+    "id": "11",
+    "name": "Dilip"
+}
+console.log(Array.of(score1,name,details))
+
