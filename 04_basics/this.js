a7d5abc4bf6d68f842e18e620097188baeb0058c
mukherjeesourish36 <86368695+mukherjeesourish36@users.noreply.github.com>
2026-04-25 15:30:18 +0000

    thissss

--- 04_basics/this.js
+++ 04_basics/this.js
@@ -1,14 +1,24 @@
-const user = {
-    username : "Sourish",
-    place : "Kolkata",
-    welcomeMessage : function(){
-     console.log(`${this.username}, is from ${this.place}`)
-     console.log(this)
-    }
+// const user = {
+//     username : "Sourish",
+//     place : "Kolkata",
+//     welcomeMessage : function(){
+//      console.log(`${this.username}, is from ${this.place}`)
+//      console.log(this)
+//     }
+// }
+
+// // user.welcomeMessage()
+// // user.username = "Tripti"
+// // user.welcomeMessage()
+
+// console.log(this)
+
+function chai()
+{
+    // let username = "Sourish"
+    username = "Sourish"
+    console.log(this.username)
+    console.log(this)
 }
 
-// user.welcomeMessage()
-// user.username = "Tripti"
-// user.welcomeMessage()
-
-console.log(this)
+chai()
