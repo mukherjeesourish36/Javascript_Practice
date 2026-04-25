135c3288efc721eb08716efe1431254effbfcc34
mukherjeesourish36 <86368695+mukherjeesourish36@users.noreply.github.com>
2026-04-25 11:01:36 +0000

    arrfunc

--- 04_basics_Function/func1.js
+++ 04_basics_Function/func1.js
@@ -11,13 +11,24 @@ function shoppingCart(...items)
 //    place : "Kolkata"
 // }
 
-function userDetails(anyobject)
-{
-    console.log(`User's id is ${anyobject.id} whose name is ${anyobject.name} and he lives in ${anyobject.place}`)
-}
+// function userDetails(anyobject)
+// {
+//     console.log(`User's id is ${anyobject.id} whose name is ${anyobject.name} and he lives in ${anyobject.place}`)
+// }
 
-userDetails({
-   id : 17,
-   name : "Sourish",
-   place : "Kolkata"
-});
+// userDetails({
+//    id : 17,
+//    name : "Sourish",
+//    place : "Kolkata"
+// });
+
+// const arr = [200, 300, 400, 500]
+
+// function arrSecond(anyArray)
+// {
+//    //  return anyArray[1]
+//    console.log("2nd array element is :",anyArray[1])
+// }
+
+// arrSecond(arr)
+
