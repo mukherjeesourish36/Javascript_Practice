2763f36960e4273c5fe387ac322ff1bfa9c9517e
mukherjeesourish36 <86368695+mukherjeesourish36@users.noreply.github.com>
2026-04-25 12:33:43 +0000

    if scope

--- 04_basics/scope.js
+++ 04_basics/scope.js
@@ -1,14 +1,44 @@
-let a = 100;
-var c = 900;
+// let a = 100;
+// var c = 900;
+
+// if(true)
+// {
+//     let a = 10
+//     const b = 20
+//     var c = 90
+//     console.log("inner let variable",a)
+//     console.log("inner var variable",c)
+// }
+
+// console.log(a);
+// console.log(c);
+
+
+
+// function one()
+// {
+//     const username = "Sourish";
+
+//     function two()
+//     {
+//         const website = "Youtube";
+//         console.log(username);
+//     }
+
+//     two();
+
+//     //console.log(wesbite);
+// }
+// one();
 
 if(true)
 {
-    let a = 10
-    const b = 20
-    var c = 90
-    console.log("inner let variable",a)
-    console.log("inner var variable",c)
+    const username = "Sourish" 
+    if(username === "Sourish")
+    {
+        const website = " Youtube"
+        console.log(username + website)
+    }
+    //console.log(website)
 }
-
-console.log(a);
-console.log(c);
+//console.log(username)
