8d971522dd299ad9f0d2c36d5727e2280dc64764
mukherjeesourish36 <86368695+mukherjeesourish36@users.noreply.github.com>
2026-04-25 11:03:06 +0000

    arr func1

--- 04_basics_Function/func1.js
+++ 04_basics_Function/func1.js
@@ -22,13 +22,15 @@ function shoppingCart(...items)
 //    place : "Kolkata"
 // });
 
-// const arr = [200, 300, 400, 500]
+const arr = [200, 300, 400, 500]
 
-// function arrSecond(anyArray)
-// {
-//    //  return anyArray[1]
-//    console.log("2nd array element is :",anyArray[1])
-// }
+function arrSecond(anyArray)
+{
+   //  return anyArray[1]
+   console.log("2nd array element is :",anyArray[1])
+}
 
 // arrSecond(arr)
+
+arrSecond([200, 300, 400, 500])
 
