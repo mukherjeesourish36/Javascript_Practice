0cb83a51b145d475dadb8c49f6ed643375c68a25
mukherjeesourish36 <86368695+mukherjeesourish36@users.noreply.github.com>
2026-04-25 14:45:58 +0000

    arww

---
+++ 04_basics/arrow.js
@@ -0,0 +1,11 @@
+// const addTwo = (num1,num2) => {
+//     return num1 + num2;
+// }
+
+// const addTwo = (num1,num2) => num1 + num2;
+
+// const addTwo = (num1,num2) => ( num1 + num2 );
+
+const addTwo = (num1,num2) => ({username : "Sourish"});
+
+console.log(addTwo(4,3))
