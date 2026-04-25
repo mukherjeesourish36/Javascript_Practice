a5cab85b5d1ac4609b12909ceb5cbef9f1877ad8
mukherjeesourish36 <86368695+mukherjeesourish36@users.noreply.github.com>
2026-04-25 08:22:02 +0000

    func1

--- 04_basics_Function/func1.js
+++ 04_basics_Function/func1.js
@@ -3,4 +3,17 @@ function shoppingCart(...items)
    return items
 }
 
-console.log(shoppingCart("Soap","Oil","Egg","Toys"))
+// console.log(shoppingCart("Soap","Oil","Egg","Toys"))
+
+const user = {
+   id : 17,
+   name : "Sourish",
+   place : "Kolkata"
+}
+
+function userDetails(anyobject)
+{
+    console.log(`User's id is ${anyobject.id} whose name is ${anyobject.name} and he lives in ${anyobject.place}`)
+}
+
+userDetails(user);
