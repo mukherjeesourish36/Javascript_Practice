d72f8fe26d82cda2cce0269bb53cc0462825d363
mukherjeesourish36 <86368695+mukherjeesourish36@users.noreply.github.com>
2026-04-28 09:06:39 +0000

    iife2

--- 04_basics/iife.js
+++ 04_basics/iife.js
@@ -1,9 +1,18 @@
-var count = 10;
+// var count = 10;
 
-function doSomething() {
-  count = 20; // local box
-  console.log(count); // prints 20 (local only)
-}
+// function doSomething() {
+//   count = 20; // local box
+//   console.log(count); // prints 20 (local only)
+// }
 
-doSomething();
-console.log(count); // prints 10 (global unchanged)
+// doSomething();
+// console.log(count); // prints 10 (global unchanged)
+
+(function chai()
+{
+    console.log("hmm!! the chai is good in taste")
+})();
+
+( (name) => {
+   console.log(`hmm!! I am ${name}, the coffee is good in taste`)
+})("Sourish")
