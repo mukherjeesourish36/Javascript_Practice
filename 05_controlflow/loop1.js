2497a24d042cdf6c1af219ad9b7c90f46d4bad15
mukherjeesourish36 <86368695+mukherjeesourish36@users.noreply.github.com>
2026-04-29 20:35:09 +0000

    iii

--- 05_controlflow/loop1.js
+++ 05_controlflow/loop1.js
@@ -4,12 +4,30 @@
 //         console.log(element);
 // }
 
-for(let index=1; index<=10; index++)
+// for(let index=1; index<=10; index++)
+// {
+//         let element=index;
+//         if(element == 5)
+//         {
+//             console.log("5 is the best number");
+//         }
+//         console.log(element);
+// }
+
+// for(let i=0; i<=10; i++)
+// {
+//     console.log(`this is outer loop with ${i}`)
+
+//     for(let j=0; j<=10; j++)
+//     {
+//         console.log(`this is inner loop with ${j}`)
+//     }
+// }
+
+const arr = ["Sourish","Soumen","Alok"];
+
+for(let i=0; i<arr.length; i++)
 {
-        let element=index;
-        if(element == 5)
-        {
-            console.log("5 is the best number");
-        }
-        console.log(element);
+    let element = arr[i];
+    console.log(element)
 }
