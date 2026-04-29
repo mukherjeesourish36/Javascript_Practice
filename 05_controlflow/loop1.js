3309724900221e8199c0b3c98d4e977a2d72bdfb
mukherjeesourish36 <86368695+mukherjeesourish36@users.noreply.github.com>
2026-04-29 18:57:36 +0000

    mm

--- 05_controlflow/loop1.js
+++ 05_controlflow/loop1.js
@@ -1,5 +1,15 @@
+// for(let index=1; index<=10; index++)
+// {
+//         let element=index;
+//         console.log(element);
+// }
+
 for(let index=1; index<=10; index++)
 {
         let element=index;
+        if(element == 5)
+        {
+            console.log("5 is the best number");
+        }
         console.log(element);
 }
