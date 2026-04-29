af268815a285073f2c9dd86a87bec2c73de9a706
mukherjeesourish36 <86368695+mukherjeesourish36@users.noreply.github.com>
2026-04-29 21:02:08 +0000

    jjj

---
+++ 05_controlflow/loop2.js
@@ -0,0 +1,14 @@
+// let index=0;
+// while(index <= 10)
+// {
+//    console.log(index)
+//    index=index+2;
+// }
+
+// const arr = ["cts","tcs","wipro"]
+// let i=0;
+// while(i < arr.length)
+// {
+//     console.log(arr[i])
+//     i++;
+// }
