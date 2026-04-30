72392bd7601277b16b2d781318e850b828ec9e8e
mukherjeesourish36 <86368695+mukherjeesourish36@users.noreply.github.com>
2026-04-30 20:14:38 +0000

    lp

---
+++ 05_controlflow/loop3.js
@@ -0,0 +1,12 @@
+// const num = [1, 2, 3, 4, 5]
+
+// for (const i of num) {
+//     console.log(i)
+// }
+
+// const name = "Hello Sourish"
+
+// for (const n of name) {
+//     console.log(`each character is ${n}`)
+//     console.log("n")
+// }
