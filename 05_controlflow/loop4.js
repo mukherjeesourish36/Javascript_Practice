327849382f01f401620058a8559f06437cc12dcf
mukherjeesourish36 <86368695+mukherjeesourish36@users.noreply.github.com>
2026-04-30 23:12:02 +0000

    tttt

--- 05_controlflow/loop4.js
+++ 05_controlflow/loop4.js
@@ -8,8 +8,27 @@ const prog = ["Java","C","C#","JS"]
 //     console.log(i)
 // })
 
-function printMe(item) {
-  console.log(item);
-}
+// function printMe(item) {
+//   console.log(item);
+// }
 
-prog.forEach(printMe);
+// prog.forEach(printMe);
+
+
+const students = [
+  { name: "Sourish", age: 28, city: "Kolkata" },
+  { name: "Tanvir", age: 22, city: "Delhi" },
+  { name: "Riya", age: 25, city: "Mumbai" }
+];
+
+// students.forEach((item) => {
+// console.log(item.name)
+// console.log(item)
+// })
+
+// for (const {name,age,city} of students) {
+//     console.log(`${name} of ${age} years old lives in ${city} city`)
+// }
+
+const [ name, age, city ] = students;
+ console.log(`${name} of ${age} years old lives in ${city} city`)
