d46eb29ad882b518b48cf64b53a6b86049ad64cd
mukherjeesourish36 <86368695+mukherjeesourish36@users.noreply.github.com>
2026-04-30 21:16:39 +0000

    mapp

--- 05_controlflow/loop3.js
+++ 05_controlflow/loop3.js
@@ -10,3 +10,34 @@
 //     console.log(`each character is ${n}`)
 //     console.log("n")
 // }
+
+const countries = new Map()
+
+countries.set("IN","India")
+countries.set("UK","United Kingdom")
+countries.set("RUS","Russia")
+countries.set("IL","Israel")
+
+// console.log(countries.get("IN"))
+// console.log(countries.get("RUS"))
+
+// console.log(countries.has("IN"))
+// console.log(countries.has("PAK"))
+
+// console.log(countries.size)
+
+// countries.delete("UK")
+
+// for (const [key,value] of countries) {
+//     console.log(`${key} => ${value}`)
+// }
+
+const user1 = {"id":1}
+const user2 = {"id":2}
+
+const s = new Map()
+
+s.set(user1, "Sourish")
+s.set(user2, "Sanjit")
+
+console.log(s.get(user1))
