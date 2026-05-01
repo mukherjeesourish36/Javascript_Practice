b23cba15588e9baa34accd8be5ddc54d9195c903
mukherjeesourish36 <86368695+mukherjeesourish36@users.noreply.github.com>
2026-05-01 09:12:39 +0000

    ll

--- 05_controlflow/loop5.js
+++ 05_controlflow/loop5.js
@@ -2,8 +2,17 @@ const num = [1,2,3,4,5,6,7,8,9,10]
 
 // const newNum = num.filter((i) => i>4)
 
-const newNum = num.filter((i) => {
-    return i>4
-})
+// const newNum = num.filter((i) => {
+//     return i>4
+// })
 
-console.log(newNum)
+// newNum = []
+
+// num.forEach( (n) => {
+//     if(n>4)
+//     {
+//         newNum.push(n);
+//     }
+// })
+
+// console.log(newNum)
