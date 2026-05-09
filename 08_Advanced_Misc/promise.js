06cf0a53b1825fc211dee201d725f75ebc8a5960
mukherjeesourish36 <86368695+mukherjeesourish36@users.noreply.github.com>
2026-05-09 13:29:30 +0000

    pr2

--- 08_Advanced_Misc/promise.js
+++ 08_Advanced_Misc/promise.js
@@ -17,3 +17,16 @@ new Promise(function(resolve,reject){
 }).then(function(){
     console.log("Promise 2 consumed..");
 });
+
+
+const promiseThree = new Promise(function(resolve,reject){
+       setTimeout(function() {
+             console.log("This is task 3");
+             resolve({name:"Sourish",email:"skm@example.com"});
+       },1000);
+});
+
+promiseThree.then(function(obj){
+   console.log(obj);
+   console.log("Promise 3 consumed..");
+})
