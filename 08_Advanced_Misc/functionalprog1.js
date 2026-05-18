7943882db5c276ad7f9ae274af59894276a18b78
mukherjeesourish36 <86368695+mukherjeesourish36@users.noreply.github.com>
2026-05-18 13:40:52 +0000

    func1

--- 08_Advanced_Misc/functionalprog1.js
+++ 08_Advanced_Misc/functionalprog1.js
@@ -1,35 +1,59 @@
 const radius = [3, 1, 2, 4];
 
-const calculateArea = function (radius) {
-  const output = [];
+const area = function (radius){
+  return Math.PI * radius * radius;
+};
+
+const circum = function (radius){
+  return 2 * Math.PI * radius;
+};
+
+const dia = function (radius){
+  return 2 * radius;
+};
+
+const calculate = function (radius,logic) {
+ const output = [];
   for (let i = 0; i < radius.length; i++) {
-    output.push(Math.PI * radius[i] * radius[i])
+    output.push(logic(radius[i]))
   }
   return output;
 };
 
-console.log(calculateArea(radius));
+console.log(calculate(radius,area));
+console.log(calculate(radius,circum));
+console.log(calculate(radius,dia));
 
-const calculateCircum = function (radius) {
-  const output = [];
-  for(let i = 0; i < radius.length; i++)
-  {
-    output.push(2 * Math.PI * radius[i]);
-  }
-  return output;
-};
+// const calculateArea = function (radius) {
+//   const output = [];
+//   for (let i = 0; i < radius.length; i++) {
+//     output.push(Math.PI * radius[i] * radius[i])
+//   }
+//   return output;
+// };
 
-console.log(calculateCircum(radius));
+// console.log(calculateArea(radius));
 
-const calculateDia = function (radius) {
-  const output = [];
-  for(let i = 0; i < radius.length; i++)
-  {
-    output.push(2 * radius[i]);
-  }
-  return output;
-};
+// const calculateCircum = function (radius) {
+//   const output = [];
+//   for(let i = 0; i < radius.length; i++)
+//   {
+//     output.push(2 * Math.PI * radius[i]);
+//   }
+//   return output;
+// };
 
-console.log(calculateDia(radius));
+// console.log(calculateCircum(radius));
+
+// const calculateDia = function (radius) {
+//   const output = [];
+//   for(let i = 0; i < radius.length; i++)
+//   {
+//     output.push(2 * radius[i]);
+//   }
+//   return output;
+// };
+
+// console.log(calculateDia(radius));
 
 
