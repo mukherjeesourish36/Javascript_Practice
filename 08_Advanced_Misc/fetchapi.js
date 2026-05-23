8f0b716f3b4aa9a7c0e55d269f5f91d9aa575491
Sourish <mukherjee.sourish36@gmail.com>
2026-05-23 21:26:14 +0530

    ft
    Signed-off-by: Sourish <mukherjee.sourish36@gmail.com>

--- 08_Advanced_Misc/fetchapi.js
+++ 08_Advanced_Misc/fetchapi.js
@@ -1,13 +1,15 @@
-const getApi = "https://isro.vercel.app/api/spacecrafts";
+const getApi = "https://catfact.ninja/facts";
+//const paraData = document.querySelector("#para");
 
 const getData = async () => {
      let res = await fetch(getApi);
-      console.log(res);
-    //  let data = await res.json();
-    //  console.log(data);
+    // console.log(res);
+     let data = await res.json();
+     console.log(data);
+    console.log(data.data[0].fact);
+   // paraData.innerText=data.data[0].fact;
 
-    console.log(await res.json());
+    //console.log(data);
     
 };
-
 getData();
